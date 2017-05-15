d4224c776d089e440c24109714a868b76ce4aef1
Alex75 <alessandro.piccione.75@gmail.com>
2017-05-15 18:37:21 +0100

    Add Unit Tests and Integration Tests projects.

---
+++ Integration Tests/DataAccess/Repositories/PostCodeRepositoryTest.cs
@@ -0,0 +1,46 @@
+﻿using MongoDB.Driver;
+using NUnit.Framework;
+using PropertyWizard.DataAccess.Entities;
+using PropertyWizard.WebApiDataAccess.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyWizard.IntegrationTests.DataAccess.Repositories
+{
+    [TestFixture, Category("PostCode")]
+    public class PostCodeRepositoryTest
+    {
+        private PostCodeRepository repository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            repository = new PostCodeRepository();
+        }
+
+        [Test]
+        public void List()
+        {
+
+            CreatePostCode(new PostCode("Test 1", DateTime.UtcNow.ToString()));
+
+            // Execute
+            var list = repository.List();
+
+            Assert.IsNotNull(list);
+            Assert.IsNotEmpty(list);
+        }
+
+        private void CreatePostCode(PostCode postCode)
+        {
+            var collection = new MongoClient(ConfigurationManager.AppSettings["MongoDB connection string"]).GetDatabase("property-wizard").GetCollection<PostCode>("postcodes");
+
+            collection.InsertOne(postCode);
+        }
+
+    }
+}
