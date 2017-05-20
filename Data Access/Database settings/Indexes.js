7af68f25e6b8db9ec6489618a7acbac8134ef541
Alex75 <alessandro.piccione.75@gmail.com>
2017-05-20 17:10:09 +0100

    Add file with database indexes definition.

---
+++ Data Access/Database settings/Indexes.js
@@ -0,0 +1,13 @@
+﻿use property_wizard
+
+db.getCollection("postcodes")
+    .createIndex(
+        { "code": 1}, {unique: true}
+    );
+    
+
+db.getCollection("zoopla-listings")
+    .createIndex(
+        { "listing_id": 1}, {unique: true}
+    );
+
