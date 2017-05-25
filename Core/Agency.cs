677d44bac5ca5efc281120d1ad7bafa55e995395
Alex75 <alessandro.piccione.75@gmail.com>
2017-05-25 20:44:07 +0100

    Create Core project using .Net Core (with .Net Framework 4.6.1). Add AgencyProvider class.

---
+++ Core/Agency.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PropertyWizard.WebApiDataAccess.Core
+{
+    public class Agency
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        //public string LongName { get; set; }
+
+
+        public Agency(string code, string name)
+        {
+            Code = code;
+            Name = name;
+        }
+
+        //public static Agency Parse(string description)
+        //{
+        //    if(description == )
+        //}
+
+    }
+}
---
+++ Core/AgencyProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PropertyWizard.WebApiDataAccess.Core
+{
+    public class AgencyProvider
+    {
+        private static List<Agency> agencies;
+
+        public List<Agency> GetAllAgencies()
+        {
+            if (agencies == null)
+                LoadAgencies();
+
+            return agencies;
+        }
+
+        private void LoadAgencies()
+        {
+            agencies = new List<Agency>() {
+                new Agency("Fox1", "Foxtons - London Bridge"),
+                new Agency("Cha1", "Chase Evans City & Aldgate"),
+                new Agency("Atk1", "Atkinson Mcleod - Kennington"),
+                new Agency("Nel1", "Nelsons"),
+            };
+        }
+    }
+}
